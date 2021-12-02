161c7fe205d0fd8ae1eac25c2c01c14baeb0b59b
Hallam Rear <hallamrear@gmail.com>
2021-12-02 13:53:40 +0000

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDepthIncreases = 0;
+            int[] data = Array.ConvertAll(File.ReadAllLines(@"Data.txt"), int.Parse);
+
+            for(int i = 1; i < data.Length; i++)
+            {
+                if(data[i] > data[i - 1])
+                    numberOfDepthIncreases++;
+            }
+
+            Console.WriteLine("Depth increases: " + numberOfDepthIncreases);
+            Console.ReadLine();
+        }
+    }
+}
